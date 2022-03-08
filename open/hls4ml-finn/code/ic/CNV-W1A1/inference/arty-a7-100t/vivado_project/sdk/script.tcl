0e9cc2c97d1cdb97ed5cf044aa26a2e22019f295
Hendrik Borras <hendrikborras@web.de>
2022-03-08 10:45:10 +0100

    Make Arty SDK create c++ app

--- open/hls4ml-finn/code/ic/CNV-W1A1/inference/arty-a7-100t/vivado_project/sdk/script.tcl
+++ open/hls4ml-finn/code/ic/CNV-W1A1/inference/arty-a7-100t/vivado_project/sdk/script.tcl
@@ -5,7 +5,7 @@ setws .
 if { $::argc == 1 } {
     set accname [lindex $::argv 0]
     createhw -name $accname\_platform -hwspec hdf/$accname\_design_wrapper.hdf
-    createapp -name $accname\_standalone -app {Hello World} -proc microblaze_mcu -hwproject $accname\_platform -os standalone
+    createapp -name $accname\_standalone -lang c++ -app {Empty Application} -proc microblaze_mcu -hwproject $accname\_platform -os standalone
     configapp -app $accname\_standalone build-config release
     configapp -app finn_cnv_w1a1_standalone -add linker-misc {-Wl,--defsym=_HEAP_SIZE=0x1000000}
     configapp -app finn_cnv_w1a1_standalone -add linker-misc {-Wl,--defsym=_STACK_SIZE=0x40000}
